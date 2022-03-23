fa030178cf43acda102151edd3af90003f9a08c6
ademaes <amaldonade@eafit.edu.co>
2022-03-23 14:58:10 -0500

    implementacion pagina principal discapacitados

--- inncicla/settings.py
+++ inncicla/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'InnciclaApp',
+    "bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -119,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static'),
+MEDIA_URL='/images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
