3753a961378b4911a9219fc054bcc76fbf3285bd
María Camila Morales Ríos <mcmoralesr@eafit.edu.co>
2022-03-24 02:51:09 -0500

    Authentication + HTTP data
    Los usuarios deberían poder consultar el servidor para obtener información sobre sus viajes pasados, presentes y futuros. La información actualizada es vital para comprender desde dónde han viajado los usuarios y para planificar hacia dónde viajarán

--- Inncicla-app/server/bycicle/settings.py
+++ Inncicla-app/server/bycicle/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os # add with the other imports
-
+import datetime
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -147,4 +147,17 @@ CHANNEL_LAYERS = {
             'hosts': [REDIS_URL],
         },
     },
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
+    'USER_ID_CLAIM': 'id',
 }
